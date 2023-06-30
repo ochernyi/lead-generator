fcf37a8bf8a2528e1a1fbfd12d509dd676f090e4
Oleh Chornyi <ochernyi04@gmail.com>
2023-06-30 17:42:46 +0300

    Add forms for Login and SignUp

---
+++ lead/forms.py
@@ -0,0 +1,68 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from lead.models import User
+
+
+class LeadForm(forms.Form):
+    number_choices = (
+        ('Ten', 10),
+        ('Twenty', 20),
+        ('Thirty', 30),
+    )
+
+    keywords = forms.CharField(label="Keywords: ")
+    location = forms.CharField(label="Locations: ")
+    leads_num = forms.IntegerField(label="Leads num: ", widget=forms.Select(choices=number_choices))
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control"
+            }
+        ))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+                "class": "form-control"
+            }
+        ))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "Email",
+                "class": "form-control"
+            }
+        ))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password check",
+                "class": "form-control"
+            }
+        ))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
